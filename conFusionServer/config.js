aec0ac2bbeb7041ad98bf64d0c3e00a3705e74f0
abc <prajapatijimil@yahoo.com>
2020-06-06 13:16:01 +0530

    facebook oauth2

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+        clientId: '271540287285885',
+        clientSecret: '0fa94330bc645a9f076039254056410c'
+    }
 }
